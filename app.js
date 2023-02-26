d35b7f6ea188842c7b7e00050390a0bb04bda4f8
MuhammadSubhan45 <buex9xx@gmail.com>
2023-02-26 11:15:21 +0500

    updated

---
+++ app.js
@@ -0,0 +1,9 @@
+// function toggleNav() {
+//     var x = document.getElementsByClassName("nav-links")[0];
+//     if (x.style.display === "flex") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "flex";
+//     }
+//   }
+  
